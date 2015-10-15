6acf638d401460097b7b040a9084fcdf3796d8b6
cricciar <cricciar@pratt.edu>
2015-10-15 10:47:18 +0000

    Adding index file

--- Random/script.js
+++ Random/script.js
@@ -1,14 +1,14 @@
+//begin
+
 var random_images = ["Image1.png", "Image2.png", "Image3.png", "Image4.png", "Image5.png"];
+
+//randomize images
 
 $(".toggle").click(function() {
     
 var randomimg = random_images[Math.floor(Math.random()*random_images.length)];
 
 
-$(".images").html("<img src='img/"+ randomimg + "'>")
-$(".images").html("<img src='img/"+ randomimg + "'>")
-$(".images").html("<img src='img/"+ randomimg + "'>")
-$(".images").html("<img src='img/"+ randomimg + "'>")
 $(".images").html("<img src='img/"+ randomimg + "'>")
 $(".images").html("<img src='img/"+ randomimg + "'>")
 $(".images").html("<img src='img/"+ randomimg + "'>")
