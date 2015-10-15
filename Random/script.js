0ad08add691592650803ff79c942615b3298db8b
cricciar <cricciar@pratt.edu>
2015-10-15 11:01:07 +0000

    Adding index file

--- Random/script.js
+++ Random/script.js
@@ -17,3 +17,34 @@ $(".images").html("<img src='img/"+ randomimg + "'>")
 
 
 });
+
+
+//circles
+
+function getRandomColor() {
+    var colours = ["#00c0f1", "#add036", "#ec2426", "#ffc116"];
+    return colours[Math.floor(Math.random() * 4)]
+}
+
+var maxDiam = 50;
+var circleNum = 200;
+var container = $("#container")
+var containerWidth = container.width();
+var containerHeight = container.height();
+
+$(document).ready(function() {
+    for (var i = 0; i < circleNum; i++) {
+        var newCircle = $("<div />")
+        var d = Math.floor(Math.random() * maxDiam);
+        newCircle.addClass("circle");
+
+        newCircle.css({
+            width: d,
+            height: d,
+            left: Math.random() * Math.max(containerWidth - d, 0),
+            top: Math.random() * Math.max(containerHeight - d, 0),
+            backgroundColor: getRandomColor()
+        });
+        container.append(newCircle);
+    }
+});
